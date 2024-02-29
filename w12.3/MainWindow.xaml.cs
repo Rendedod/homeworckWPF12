b19cdfa870247ac8ec069f96c4f9822c0ca88358
Rended <ilyaromanovich2024@mail.ru>
2024-03-01 00:38:43 +0700

    Добавьте файлы проекта.

---
+++ w12.3/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace w12._3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void But_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            But.Background = new SolidColorBrush(Color.FromRgb(183, 94, 94));
+        }
+
+        private void But_MouseLeave(object sender, MouseEventArgs e)
+        {
+            But.Background = new SolidColorBrush(Color.FromRgb(154, 66, 66));
+        }
+
+        private void But2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            But2.Background = new SolidColorBrush(Color.FromRgb(183, 94, 94));
+        }
+
+        private void But2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            But2.Background = new SolidColorBrush(Color.FromRgb(154, 66, 66));
+        }
+
+        private void But_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            long number = Convert.ToInt64(label1.Content) - 1;
+            label1.Content = number;
+        }
+
+        private void But2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            long number = Convert.ToInt64(label1.Content) + 1;
+            label1.Content = number;
+        }
+    }
+}
---
+++ worckWPF12/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace worckWPF12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void But_MouseMove(object sender, MouseEventArgs e)
+        {
+            But.Background = new SolidColorBrush(Color.FromRgb(183, 94, 94));
+        }
+
+        private void But_MouseLeave(object sender, MouseEventArgs e)
+        {
+            But.Background = new SolidColorBrush(Color.FromRgb(154, 66, 66));
+        }
+
+        private void But_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Кнопка была нажата");
+        }
+    }
+}
